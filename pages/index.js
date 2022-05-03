ab00d347f54cefa4cf2e67b030b2f07c8deeb844
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-05-04 00:56:01 +0100

    sidebar

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Sidebar from "../components/Sidebar";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="bg-black h-screen overflow-hidden">
+      <main>
+        <Sidebar />
+        {/* center */}
+      </main>
+      <div>{/* player */}</div>
+    </div>
+  );
 }
