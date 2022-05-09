62c25a7bf438ad40b187d5101b075eac4ac438c1
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-05-09 03:01:09 +0100

    adding middleware

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -7,9 +7,11 @@ import {
   HeartIcon,
   LogoutIcon,
 } from "@heroicons/react/outline";
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 
 function Sidebar() {
+  const { data: session, status } = useSession();
+
   return (
     <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
       <div className="space-y-4">
